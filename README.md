ca66d7b85ad2ca59df9884a76e3606e810405578
KQT3 <70701718+KQT3@users.noreply.github.com>
2023-04-18 11:07:27 +0200

    commit

---
+++ README.md
@@ -0,0 +1,4 @@
+npm i
+npm run build
+
+sudo npm link (/usr/lib/node_modules/npm-library
---
+++ src/components/mui/SenseraButton.tsx
@@ -0,0 +1,7 @@
+import Button from '@mui/material/Button';
+
+export const SenseraButton = () => {
+  return (
+    <Button variant="contained">Hello World</Button>
+  );
+}
